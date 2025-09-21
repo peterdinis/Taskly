2c9295d4351509ffec5c06d6ee26c7d3da209c81
peterdinis <petter1@pobox.sk>
2025-09-21 17:19:31 +0200

    feat: Design dashboard page WIP

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const DashboardPage: NextPage = () => {
+    return (
+        <>DAshboard</>
+    )
+}
+
+export default DashboardPage
---
+++ components/dashboard/DashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const DashboardWrapper: FC = () => {
+    return (
+        <>ABCD</>
+    )
+}
+
+export default DashboardWrapper;
